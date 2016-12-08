f4d45b89181b7c6e9f839a8086b4657aa3579715
David Leonard <davidrobertleonard@gmail.com>
2016-12-08 11:34:16 -0800

    WIP -- partway through adding conflict finder, but likely will want to change a bunch of this. Checking in as we go.

--- examples/findConflicts.js
+++ examples/findConflicts.js
@@ -2,15 +2,19 @@
 
 const Promise = require('bluebird');
 const DepIndex = require('../index');
+const semver = require('semver');
 
 const deps = DepIndex({
   projectsDirPath: __dirname + '/../repos',
   printResults: true
 });
 
-deps.search('px-defaults-design')
+deps.findGlobalConflicts()
   .then((result) => {
     console.log('Done!');
+    const v = require("../package.json").version;
+    const f = semver.clean('^3.2.1');
+    debugger;
     // `result` is a Map. Check if it has a size (like calling .length on an array). If it does, we found a matching module.
     // if (result && result.size) {
     //   console.log(JSON.stringify([...result], null, 4)); // result is a map, coerce its keys/values before passing to JSON stringify
